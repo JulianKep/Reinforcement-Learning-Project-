ac0de9bb8a667f3abcb54a1c46bd71eb71422d97
julian <ledoho41@gmail.com>
2026-05-15 15:38:41 +0200

    sheet u

--- sheet1/main.py
+++ sheet1/main.py
@@ -1,5 +1,12 @@
 import numpy as np
 import math
+import random
+import time
+
+def state_to_coordinate(current_state, square_dim):
+    x = math.floor(square_dim / current_state)
+    y = square_dim % current_state
+    return(x,y)
 
 square_dim = 4
 discount_factor = 0.8
@@ -9,25 +16,46 @@ arr = np.full((square_dim, square_dim), -1)
 arr[0][0] = 0
 arr[square_dim-1][square_dim-1] = 0
 
-
-""" states = np.full(square_dim, )
-states[0] =  """
+print("initial array:\n", arr)
 
 #set initial state to bottom left state
 current_state = square_dim * (square_dim -1)
 
+print("initial state:", current_state)
+
+
 terminal_states = [0, square_dim ** 2 - 1]
+
 
 
 while current_state not in terminal_states:
 
-    #get coordiantes
-    x = math.floor(square_dim / current_state)
-    y = square_dim % current_state
+    x = current_state // square_dim
+    y = current_state % square_dim
 
-    print(x, y)
-    break
+    direction = random.randint(0, 3)
 
+    match direction:
+        case 0:  # up
+            if x - 1 >= 0:
+                x -= 1
+
+        case 1:  # right
+            if y + 1 <= square_dim -1:
+                y += 1
+
+        case 2:  # down
+            if x + 1 <= square_dim -1:
+                x += 1
+
+        case 3:  # left
+            if y - 1 >= 0:
+                y -= 1
+
+    current_state = x * square_dim + y
+
+    time.sleep(0.5)
+    print(x, y, current_state)
 
 
 print(arr)
